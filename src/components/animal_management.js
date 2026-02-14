684330972c6add6667fec933654d8975eff90b44
Yaganti.Spandana <spandanayaganti2@gmail.com>
2026-02-14 22:24:02 +0530

    Update animal_management.js

--- src/components/animal_management.js
+++ src/components/animal_management.js
@@ -36,8 +36,14 @@ export default function AnimalForm() {
   };
 
   useEffect(() => {
-    fetchAnimals();
-  }, []);
+  const fetchAnimals = async () => {
+    const res = await fetch('/api/animals');
+    const data = await res.json();
+    setAnimals(data);
+  };
+
+  fetchAnimals();
+}, []);
 
   const handleChange = (e) => {
     setAnimal({ ...animal, [e.target.name]: e.target.value });
