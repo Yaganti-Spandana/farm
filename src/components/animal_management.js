4886b06d6fbbfd1111f829ec1ba8ea903f61c2e5
Yaganti.Spandana <spandanayaganti2@gmail.com>
2026-02-14 16:05:02 +0530

    Update animal_management.js

--- src/components/animal_management.js
+++ src/components/animal_management.js
@@ -38,11 +38,12 @@ export default function AnimalForm() {
       milk_per_day: "",
       status: "active",
     });
-    catch (error) {
-  console.log(error.response.data);
-}
+   
     navigate("/animal");   // go back to animal list
   };
+   catch (error) {
+  console.log(error.response.data);
+}
 
   return (
     <>
