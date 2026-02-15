b0c1f47de8619488fc4d6277c19d0a14d714333f
Yaganti.Spandana <spandanayaganti2@gmail.com>
2026-02-15 14:37:30 +0530

    Update login.js

--- src/components/navbar/login.js
+++ src/components/navbar/login.js
@@ -2,27 +2,43 @@ import { login } from "../api";
 import { useState } from "react";
 import './login.css'
 import Navbar from "../navbar/navbar"
+
 function Login() {
   const [form, setForm] = useState({username:"", password:""});
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const res = await login(form);
+
+    // store JWT + username
     localStorage.setItem("token", res.data.access);
+    localStorage.setItem("username", res.data.username);
+
     window.location.href = "/";
   };
 
   return (
     <>
-    <Navbar></Navbar>
-    <form onSubmit={handleSubmit} className="login_form">
-      <input placeholder="Username"
-        onChange={e=>setForm({...form, username:e.target.value})}/><br></br>
-      <input type="password" placeholder="Password"
-        onChange={e=>setForm({...form, password:e.target.value})}/><br></br>
-      <button>Login</button>
-    </form></>
+      <Navbar />
+      <form onSubmit={handleSubmit} className="login_form">
+        <input 
+          placeholder="Username"
+          value={form.username}
+          onChange={e=>setForm({...form, username:e.target.value})}
+        /><br/>
+
+        <input 
+          type="password" 
+          placeholder="Password"
+          value={form.password}
+          onChange={e=>setForm({...form, password:e.target.value})}
+        /><br/>
+
+        <button>Login</button>
+      </form>
+    </>
   );
 }
 
 export default Login;
+
