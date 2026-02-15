890f90766b5ab85e1f1133bdfe22e3b163ae82b5
Yaganti.Spandana <spandanayaganti2@gmail.com>
2026-02-15 10:13:50 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,12 @@
 import Navbar from "./navbar/navbar"
 import './Home.css'
 function Home(){
-    return(
-    <>
-        <div className="home">
-        <Navbar></Navbar>
-        <h2>WELCOME TO DAIRY FARM🐃🦬</h2>
-        <div/>
-    </>
-    )
+    return (
+  <div className="home">
+        <Navbar/>
+    <h2>WELCOME TO DAIRY FARM🐃🦬</h2>
+    <div />
+  </div>
+)
 }
 export default Home
