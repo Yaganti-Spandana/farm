16c5c9d43041a6293baa9f4b3c3e0736631ff548
Yaganti.Spandana <spandanayaganti2@gmail.com>
2026-02-27 14:34:06 +0530

    Update AnimalStatusChart.js

--- src/components/AnimalStatusChart.js
+++ src/components/AnimalStatusChart.js
@@ -60,7 +60,7 @@ export default function AnimalStatusChart({ month }) {
     <div style={{ width: "100%", height: 360 }}>
       <h2 style={{ textAlign: "center",color:"orange"  }}>Animal Status</h2>
 
-      <ResponsiveContainer width="100%" height={300}>
+      <ResponsiveContainer width="80%" height={300}>
         <PieChart>
           <Pie
             data={data}
