b4a8dd7933d6c1d79deac61a835c5a82ebb2c5bc
Yaganti.Spandana <spandanayaganti2@gmail.com>
2026-02-27 17:05:01 +0530

    Update navbar.js

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -22,7 +22,7 @@ function Navbar(){
   return( <div className='navbar'> {/* Hamburger */} 
     <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}> ☰ </div> 
   <div className={`nav-menu ${menuOpen ? "open" : ""}`}> {/* Home */} 
-<Link to='/' className="icon"> <HomeIcon style={{ color: "white", fontSize: "30px"}} /> <p>Home</p> </Link> 
+<Link to='/' className="icon"> <HomeIcon style={{ color: "white", fontSize: "30px"}} /> <span>Home</span> </Link> 
 {/* NEW Overview component */} {isLoggedIn && ( <Link to='/overview' className="icon"> <DashboardIcon style={{ color: "white", fontSize: "30px"}} /> <p>Overview</p> </Link> )} 
 {isLoggedIn && ( <> <Link to='/animal' className="icon"> <GiCow style={{ color: "white", fontSize: "30px"}} /> <p>Animal</p> </Link> 
   <Link to='/milk' className="icon"> <GiMilkCarton style={{ color: "white", fontSize: "30px"}} /> <p>Milk</p> </Link> 
