c424161d0661f11af9036dc462067ed467566f76
Yaganti.Spandana <spandanayaganti2@gmail.com>
2026-02-27 14:27:44 +0530

    Update MilkPieChart.js

--- src/components/MilkPieChart.js
+++ src/components/MilkPieChart.js
@@ -8,7 +8,9 @@ import {
 } from "recharts";
 
 const COLORS = ["#28a745", "#007bff", "#dc3545"];
-
+const renderLabel = ({ name, value }) => {
+  return `${name}: ${value}`;
+};
 export default function MilkPieChart({ summary }) {
   const data = [
     { name: "Milk Sold", value: summary.milk_sold || 0 },
@@ -20,7 +22,7 @@ export default function MilkPieChart({ summary }) {
     <div style={{ width: "100%", height: 360 }}>
       <h2 style={{ textAlign: "center",color:"orange"  }}>Milk Distribution</h2>
 
-      <ResponsiveContainer width="100%" height="100%">
+      <ResponsiveContainer width="100%" height="300">
         <PieChart>
           <Pie
             data={data}
@@ -29,7 +31,8 @@ export default function MilkPieChart({ summary }) {
             outerRadius={100}
             innerRadius={80}   // ✅ donut style
             paddingAngle={3}
-            label
+            label=renderLabel
+  
           >
             {data.map((entry, index) => (
               <Cell key={index} fill={COLORS[index]} />
