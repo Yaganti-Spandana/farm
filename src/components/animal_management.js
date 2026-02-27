75f83887078c38457b112970ad28be10100fde90
Yaganti.Spandana <spandanayaganti2@gmail.com>
2026-02-27 11:28:35 +0530

    Update animal_management.js

--- src/components/animal_management.js
+++ src/components/animal_management.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from "./navbar/navbar";
 import { useNavigate } from "react-router-dom";
-
+import './components.css'
 export default function AnimalManagement() {
   const navigate = useNavigate();
 
